87f3e1f5a47449ceaf8146cd49b372230c03a9e7
YordyFilpo <106267585+YordyFilpo@users.noreply.github.com>
2025-02-04 21:56:14 -0500

    Add solution for Check if One String Swap Can Make Strings Equal

---
+++ Check if One String Swap Can Make Strings Equal/Check if One String Swap Can Make Strings Equal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s2 == s1:
+            return True
+        
+        diff_indices = [i for i in range(len(s1)) if s1[i] != s2[i]]
+
+        if len(diff_indices) != 2:
+            return False
+        
+        i, j = diff_indices
+
+        return s1[i] == s2[j] and s1[j] == s2[i]
